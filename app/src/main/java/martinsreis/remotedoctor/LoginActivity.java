a43932cb8fa617ef6d587a42833fa6ef7ace9b42
Alexandre Reis <alexandreforeis@gmail.com>
2017-08-23 20:37:56 -0300

    Modificações no Login, User Area e Register
    1) adicionado findViewById em todas as  Activity
    2) botão do Login linkado com o User Area, porém não tem validação de username e password

--- app/src/main/java/martinsreis/remotedoctor/LoginActivity.java
+++ app/src/main/java/martinsreis/remotedoctor/LoginActivity.java
@@ -1,7 +1,11 @@
 package martinsreis.remotedoctor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final Button bLogin = (Button) findViewById(R.id.bLogin);
+
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(LoginActivity.this, UserAreaActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
     }
 }
+
+
+
+
--- app/src/main/java/martinsreis/remotedoctor/UserAreaActivity.java
+++ app/src/main/java/martinsreis/remotedoctor/UserAreaActivity.java
@@ -2,6 +2,8 @@ package martinsreis.remotedoctor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class UserAreaActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class UserAreaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
+
+        final TextView tvWelcomeMsg = (TextView) findViewById(R.id.tvWelcomeMsg);
+
     }
 }
